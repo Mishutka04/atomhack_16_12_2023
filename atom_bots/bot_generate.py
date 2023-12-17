bbcb0fda3ebf778369238b173d7e65bbe705c0ea
KupchinskyDev <kupchinsky.dv@zerolab.tech>
2023-12-17 09:51:00 +0300

    fix bots

--- atom_bots/bot_generate.py
+++ atom_bots/bot_generate.py
@@ -1,41 +1,40 @@
 import asyncio
 import logging
 import torch
+from aiogram.filters import Command
 from transformers import pipeline
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.filters import Command
-
 
 bot = Bot(token="6834162026:AAE4tPhJbqt5coUs1l_Y0Ooqs15mx-S83FY")
 dp = Dispatcher()
 
 model = "Denis431/docs_generate_v2"
 
+
 try:
     # Try to use CUDA
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pipe = pipeline("text-generation", model=model, device=device, max_length=500)
+    pipe = pipeline("text-generation", model=model, device=device, max_length=250)
 except Exception as e:
     # If there's an exception (e.g., CUDA not available), fall back to CPU
     print(f"An error occurred: {e}")
     print("Falling back to CPU.")
     device = torch.device("tpu")
-    pipe = pipeline("text-generation", model=model, device=device, max_length=500)
-
-
-@dp.message()
-async def echo_message(message: types.Message):
-    answer = pipe(message.text, num_return_sequences=1)[0]["generated_text"]
-    await message.answer(text=answer)
+    pipe = pipeline("text-generation", model=model, device=device, max_length=250)
 
 
 @dp.message(Command("start"))
 async def start_command(message: types.Message):
     await message.answer(
         text="Привет! Бот позволяет вам получать ответы на вопросы связанные с темой закопкок в атомной отрасли, "
-             "в том числе с нормативно-правовыми актами.\n⚡ Бот использует собственную модель, которая была дообучена "
-             "на sBERT модели.\n✉ Чтобы получить текстовый ответ, напишите свой вопрос в чат.\nУдачного пользования!")
+             "в том числе с нормативно-правовыми актами.\n⚡ Бот использует собственную модель, которая была обучена с нуля "
+             "на GPT.\n✉ Чтобы получить текстовый ответ, напишите свой вопрос в чат.\nУдачного пользования!")
+
+
+@dp.message()
+async def echo_message(message: types.Message):
+    answer = pipe(message.text, num_return_sequences=1)[0]["generated_text"]
+    await message.answer(text=answer)
 
 
 async def main():
